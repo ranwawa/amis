698c8c64c4e975e7c774a7ddae6c0612fd9fa6a3
ranqirong <274544338@qq.com>
2023-06-02 10:05:03 +0800

    feat(amis): curd新增双击事件

---
+++ packages/amis/__tests__/renderers/CRUD2.test.tsx
@@ -0,0 +1,67 @@
+/*
+ * @Description:
+ * @Date: 2023-06-02 09:49:17
+ * @Author: ranqirong 274544338@qq.com
+ */
+import {
+  cleanup,
+  fireEvent,
+  render,
+  waitFor,
+  screen
+} from '@testing-library/react';
+import '../../src';
+import {clearStoresCache, render as amisRender} from '../../src';
+import {makeEnv as makeEnvRaw, wait} from '../helper';
+import rows from '../mockData/rows';
+import type {RenderOptions} from '../../src';
+
+afterEach(() => {
+  cleanup();
+  clearStoresCache();
+  jest.useRealTimers();
+});
+
+/** 避免updateLocation里的console.error */
+const makeEnv = (env?: Partial<RenderOptions>) =>
+  makeEnvRaw({updateLocation: () => {}, ...env});
+
+describe('inner events', () => {
+  test('should call the callback function if provided while double clicking a row of the crud2', async () => {
+    const mockFn = jest.fn();
+    render(
+      amisRender(
+        {
+          type: 'crud2',
+          mode: 'table',
+          data: {
+            items: rows
+          },
+          columns: [
+            {
+              name: 'engine',
+              label: 'Rendering engine'
+            }
+          ],
+          onEvent: {
+            rowDbClick: {
+              actions: [
+                {
+                  actionType: 'custom',
+                  script: mockFn
+                }
+              ]
+            }
+          }
+        },
+        {}
+      )
+    );
+
+    await waitFor(() => {
+      const ele = screen.getAllByText('Trident');
+      fireEvent.dblClick(ele[0]);
+      expect(mockFn).toBeCalledTimes(1);
+    });
+  });
+});
